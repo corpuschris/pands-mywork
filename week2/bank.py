071313e8d204c8e165fdbc6962a9f8c0df0b2819
unknown <netofr@gmail.com>
2024-03-17 17:36:19 +0000

    bank

---
+++ week2/bank.py
@@ -0,0 +1,5 @@
+
+Amount1 = int(input('please enter the amount 1 in cents:'))
+Amount2 = int(input('please enter the amount 2 in cents:'))
+Sum = (Amount1 + Amount2) /100
+print (f' The sum of these is €{Sum}')
