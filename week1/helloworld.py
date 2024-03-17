b8046df8935b6bde9ba462e886b1f19de6213b8d
unknown <netofr@gmail.com>
2024-03-17 15:51:31 +0000

    week1

---
+++ week1/helloworld.py
@@ -0,0 +1,4 @@
+# helloworld.py
+# This program just prints out Hello World
+# author: Christiano Ferreira
+print ("Hello World")
---
+++ week2/hello.py
@@ -0,0 +1,4 @@
+# hello.py
+# This program just prints out hello 
+# author: Christiano Ferreira
+print ("Hello Chris")
